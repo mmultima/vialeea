d43a193ad906a73e4a4056d4387e03e9615ccf74
Mikko Multimäki <mikko.multimaki@kolumbus.fi>
2016-07-09 01:08:55 +0300

    Start of equipment: adding weapons.

--- controllers/equipmentController.js
+++ controllers/equipmentController.js
@@ -1,3 +1,15 @@
 app.controller('equipmentController', function ($scope) {
-    //Nothing yet
+    $scope.weapons = weapons;
+
+    if (!$scope.$parent.char.equipment) {
+        $scope.$parent.char.equipment = {};
+        if (!$scope.$parent.char.equipment.weapons) {
+            $scope.$parent.char.equipment.weapons = [];
+        }
+    }
+
+    $scope.addWeaponAndClose = function(weapon) {
+        $scope.$parent.char.equipment.weapons.push(weapon);
+        $("#weaponModal .mmclose").click();
+    };
 });
---
+++ data/weapons.js
@@ -0,0 +1,21 @@
+var weapons = 
+[
+    {
+        name: 'dagger',
+        dice: 4,
+        dicenum: 1,
+        type: 'simple'
+    },
+    {
+        name: 'longsword',
+        dice: 8,
+        dicenum: 1,
+        type: 'martial'
+    },
+    {
+        name: 'greatsword',
+        dice: 6,
+        dicenum: 2,
+        type: 'martial'
+    }
+];
