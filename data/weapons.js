56f01a4f1b78bffb7e5d8880e6b63ef95bf4543d
Mikko Multimäki <mikko.multimaki@kolumbus.fi>
2016-07-10 20:18:24 +0300

    First weapons shown in statblock.

--- data/weapons.js
+++ data/weapons.js
@@ -4,18 +4,21 @@ var weapons =
         name: 'dagger',
         dice: 4,
         dicenum: 1,
-        type: 'simple'
+        type: 'simple',
+        critrange: '19-20'
     },
     {
         name: 'longsword',
         dice: 8,
         dicenum: 1,
-        type: 'martial'
+        type: 'martial',
+        critrange: '19-20'
     },
     {
         name: 'greatsword',
         dice: 6,
         dicenum: 2,
-        type: 'martial'
+        type: 'martial',
+        critrange: '19-20'
     }
 ];
