80c2abb5b91c9547e307f7def76d781d18ff3ccf
Mikko Multimäki <mikko.multimaki@kolumbus.fi>
2016-07-14 23:16:33 +0300

    Weapons and armor added to all list.

--- data/weapons.js
+++ data/weapons.js
@@ -5,20 +5,26 @@ var weapons =
         dice: 4,
         dicenum: 1,
         type: 'simple',
-        critrange: '19-20'
+        critrange: '19-20',
+        weight: 1,
+        price: 2
     },
     {
         name: 'longsword',
         dice: 8,
         dicenum: 1,
         type: 'martial',
-        critrange: '19-20'
+        critrange: '19-20',
+        weight: 4,
+        price: 15
     },
     {
         name: 'greatsword',
         dice: 6,
         dicenum: 2,
         type: 'martial',
-        critrange: '19-20'
+        critrange: '19-20',
+        weight: 8,
+        price: 50
     }
 ];
