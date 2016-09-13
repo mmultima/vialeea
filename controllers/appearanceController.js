2632710d797077e6943515399b05427441142eeb
Mikko Multimäki <mikko.multimaki@kolumbus.fi>
2016-09-13 20:10:51 +0300

    Start of appearance data.

--- controllers/appearanceController.js
+++ controllers/appearanceController.js
@@ -1,3 +1,6 @@
 app.controller('appearanceController', function ($scope, $resource) {
+    if (!$scope.$parent.char.appearance) {
+        $scope.$parent.char.appearance = {};
+    }
     //Nothing yet.
 });
