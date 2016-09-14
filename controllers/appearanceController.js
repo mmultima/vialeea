5709d0bc2b1fbce125fa8970c3c1c7fd24cfd087
Mikko Multimäki <mikko.multimaki@kolumbus.fi>
2016-09-14 20:54:56 +0300

    Lots of appearence / PFS fields.

--- controllers/appearanceController.js
+++ controllers/appearanceController.js
@@ -2,5 +2,7 @@ app.controller('appearanceController', function ($scope, $resource) {
     if (!$scope.$parent.char.appearance) {
         $scope.$parent.char.appearance = {};
     }
-    //Nothing yet.
+    if (!$scope.$parent.char.misc) {
+        $scope.$parent.char.misc = {};
+    }
 });
