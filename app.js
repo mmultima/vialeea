e28327ce828aae6b01dcf0109075f33034074540
Mikko Multimäki <mikko.multimaki@buutti.com>
2023-07-01 23:52:05 +0300

    The connection to mongodb does something. Doesn't work but something happens.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require("express");
-var mongo = require('mongodb');
+//var mongo = require('mongodb');
 var app = express();
 var bodyParser = require('body-parser');
 
-var mongo = require('mongodb');
-
+//var mongo = require('mongodb');
+//const MongoClient = require("mongodb/lib/mongo_client");
+/*
 var Server = mongo.Server,Db = mongo.Db, BSON = mongo.BSONPure;
 
 var server = new Server('localhost', 28889, {auto_reconnect: true});
@@ -13,8 +14,75 @@ db = new Db('vialeea', server);
 db.open(function(err, db) {
     if(!err) {
         console.log("Connected to 'vialeea' database");
+    } else {
+        console.log("Connection to database failed.");
     }
 });
+*/
+/*
+var uri = "mongodb://vialeea-db:QcGXxHOMnaLJ2ej2CLPrvklVD32xlUgvmYIZ0JMLFThHB1g0EkESip3kmgVMBWvzCokMGyxkf0QJACDbdSBawQ==@vialeea-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@vialeea-db@";
+//var mongoClient = require("mongodb").MongoClient;
+MongoClient.connect(uri, function (err, db) {
+  db.close();
+});
+*/
+
+//var mongoClient = require("mongodb").MongoClient;
+/*
+mongoClient.connect("mongodb://vialeea-db:QcGXxHOMnaLJ2ej2CLPrvklVD32xlUgvmYIZ0JMLFThHB1g0EkESip3kmgVMBWvzCokMGyxkf0QJACDbdSBawQ==@vialeea-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@vialeea-db@", function (err, db) {
+    //db.use("vialeea-test");
+    //db.co
+    db.close();
+
+});
+*/ 
+/*
+async function run() {
+    try {
+      // Connect the client to the server (optional starting in v4.7)
+      await mongoClient.connect("mongodb://vialeea-db:QcGXxHOMnaLJ2ej2CLPrvklVD32xlUgvmYIZ0JMLFThHB1g0EkESip3kmgVMBWvzCokMGyxkf0QJACDbdSBawQ==@vialeea-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@vialeea-db@");
+
+      // Send a ping to confirm a successful connection
+      await mongoClient.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await mongoClient.close();
+    }
+  }
+run().catch(console.dir);
+*/
+
+var uri = "mongodb://vialeea-db:QcGXxHOMnaLJ2ej2CLPrvklVD32xlUgvmYIZ0JMLFThHB1g0EkESip3kmgVMBWvzCokMGyxkf0QJACDbdSBawQ==@vialeea-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@vialeea-db@";
+
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// Replace the placeholder with your Atlas connection string
+//const uri = "<connection string>";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
